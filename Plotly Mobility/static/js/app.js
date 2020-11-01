0c981ca134b024447efec1aa9b389f4adfe1acc6
Lisa Harrison <lmharrison0810@gmail.com>
2020-11-01 17:09:52 -0500

    Added json data to use and outlined required steps to complete

--- Plotly Mobility/static/js/app.js
+++ Plotly Mobility/static/js/app.js
@@ -1,51 +1,60 @@
 // Google Mobility Plotly Chart Selected By State
 
-Plotly.d3.csv('/data/google_mob_US.csv', function(err, rows){
+Plotly.d3.json('./data/data_us.json', function (err, rows) {
 
-        function unpack(rows, key) {
+    function unpack(rows, key) {
         return rows.map(function (row) { return row[key]; });
     }
-      
-      
-      var trace1 = {
-    type: "scatter",
-    mode: "lines",
-    name: 'Retail & Recreation',
-    x: unpack(rows, 'dates'),
-    y: unpack(rows, 'SMA_retail_recreation'),
-    line: { color: '#17BECF' }
-}
 
-var trace2 = {
-    type: "scatter",
-    mode: "lines",
-    name: 'Parks',
-    x: unpack(rows, 'dates'),
-    y: unpack(rows, 'SMA_parks'),
-    line: { color: 'red' }
-}
-var trace3 = {
-    type: "scatter",
-    mode: "lines",
-    name: 'Grocery & Pharmacy',
-    x: unpack(rows, 'dates'),
-    y: unpack(rows, 'SMA_grocery_pharmacy'),
-    line: { color: 'green' }
-}
+    function byState(state) {
+        return rows.filter(function (row) { return row.states === state; });
+    }
+    //1. get all the states from json  
 
-var trace4 = {
-    type: "scatter",
-    mode: "dotted-lines",
-    name: 'Transit',
-    x: unpack(rows, 'dates'),
-    y: unpack(rows, 'SMA_transit'),
-    line: { color: '#7F7F7F' }
-}
-var data = [trace1, trace2,trace3, trace4];
+    //2. populate the dropdown with states
 
-var layout = {
-    title: 'VA Mobility Trends',
-};
+    //3. listen for changes to the dropdown, triggering drawing graph
+    function drawPlot(rows) {
+        var trace1 = {
+            type: "scatter",
+            mode: "lines",
+            name: 'Retail & Recreation',
+            x: unpack(rows, 'dates'),
+            y: unpack(rows, 'SMA_retail_recreation'),
+            line: { color: '#17BECF' }
+        }
 
-Plotly.newPlot('myDiv', data, layout);
-      })
+        var trace2 = {
+            type: "scatter",
+            mode: "lines",
+            name: 'Parks',
+            x: unpack(rows, 'dates'),
+            y: unpack(rows, 'SMA_parks'),
+            line: { color: 'red' }
+        }
+        var trace3 = {
+            type: "scatter",
+            mode: "lines",
+            name: 'Grocery & Pharmacy',
+            x: unpack(rows, 'dates'),
+            y: unpack(rows, 'SMA_grocery_pharmacy'),
+            line: { color: 'green' }
+        }
+
+        var trace4 = {
+            type: "scatter",
+            mode: "dotted-lines",
+            name: 'Transit',
+            x: unpack(rows, 'dates'),
+            y: unpack(rows, 'SMA_transit'),
+            line: { color: '#7F7F7F' }
+        }
+        var data = [trace1, trace2, trace3, trace4];
+
+        var layout = {
+            title: 'VA Mobility Trends',
+        };
+
+        Plotly.newPlot('myDiv', data, layout);
+    }
+})
